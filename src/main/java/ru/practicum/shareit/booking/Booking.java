7e78f483c1f23b08a83b65a22fdf3981baa7a00f
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-05-08 12:22:46 +0300

    feat: add model Booking

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.booking;
 
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDate;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
 public class Booking {
+    private Long id;
+    private Item item;
+    private LocalDate startRent;
+    private LocalDate endRent;
+    private User owner;
+    private User tenant;
+    private Boolean isConfirmation;
 }
