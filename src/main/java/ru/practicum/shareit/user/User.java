e3f7970e65ab53584bcba085fbf187664445b917
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-05-08 12:38:55 +0300

    feat: add model User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class User {
+    private Long id;
+    private String name;
+    private String email;
 }
