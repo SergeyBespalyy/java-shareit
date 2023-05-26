b62fa1cd3da04d97bd28df5ff7f80cf410043de0
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-05-26 23:24:01 +0300

    feat: добавить @NoArgsConstructor @AllArgsConstructor

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import ru.practicum.shareit.booking.Status;
 
@@ -14,6 +16,8 @@ import java.time.LocalDateTime;
  */
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class BookingDto {
 
