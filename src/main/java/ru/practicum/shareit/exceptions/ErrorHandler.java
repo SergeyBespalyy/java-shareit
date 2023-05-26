4a6c98c64fa041363caeb29a84a947423a7698a1
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-05-26 19:50:52 +0300

    feat: Реализация функции бронирования. Добавить класс обработки ошибок ItemIsNotAvailableForBookingException. Добавить в ErrorHandler новые виды перехватываемых исключений

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -2,11 +2,15 @@ package ru.practicum.shareit.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.ValidationException;
+import java.sql.SQLException;
 
 /**
  * Класс описывающий ErrorHandler для централизованной обработки ошибок.
@@ -27,14 +31,49 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleValidationIdException(final ValidationIdException e) {
         log.warn("Исключение ValidationIdException {}", e.getMessage());
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse("ValidationIdException", e.getMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleSqlExceptionHelper(final SQLException e) {
+        log.warn("Ошибка ValidationException {}", e.getMessage());
+        return new ErrorResponse(e.getSQLState(), e.getMessage());
     }
 
     @ExceptionHandler(ValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(final ValidationException e) {
         log.warn("Ошибка ValidationException {}", e.getMessage());
-        return new ErrorResponse(e.getMessage());
+        return new ErrorResponse("ValidationException", e.getMessage());
+    }
+
+    @ExceptionHandler(ItemIsNotAvailableForBookingException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ItemIsNotAvailableForBookingException e) {
+        log.warn("Ошибка ItemIsNotAvailableForBookingException {}", e.getMessage());
+        return new ErrorResponse("ItemIsNotAvailableForBookingException", e.getMessage());
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingRequestHeaderException(final MissingRequestHeaderException e) {
+        log.warn("Ошибка ValidationException {}", e.getMessage());
+        return new ErrorResponse("MissingRequestHeaderException", e.getMessage());
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        log.warn("Ошибка ValidationException {}", e.getMessage());
+        return new ErrorResponse("MissingServletRequestParameterException", e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
+        log.warn("Ошибка ValidationException {}", e.getMessage());
+        return new ErrorResponse("Unknown state: UNSUPPORTED_STATUS");
     }
 }
 
