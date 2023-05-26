9a2eaa20e1b5e8e0494646d49cc981436ffa3183
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-05-26 19:44:40 +0300

    feat: Реализация функции бронирования. Добавить BookingDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,36 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+import ru.practicum.shareit.booking.Status;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 /**
- * TODO Sprint add-bookings.
+ * Класс описывает модель BookingDto
  */
+
+@Data
+@Builder
 public class BookingDto {
+
+    private Long id;
+
+    @NotNull(message = "itemId не может быть пустым")
+    private Long itemId;
+
+    @NotNull(message = "Поле start бронирования не может быть пустым")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @FutureOrPresent(message = "Дата start бронирования не может быть в прошлом")
+    private LocalDateTime start;
+
+    @NotNull(message = "Поле end бронирования не может быть пустым")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @FutureOrPresent(message = "Дата end бронирования не может быть в прошлом")
+    private LocalDateTime end;
+
+    private Status status;
 }
