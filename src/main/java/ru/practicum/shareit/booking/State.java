56d961e66d96ebed50283d4b962bfa79df873c58
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-05-26 19:59:29 +0300

    feat: добавить перечисления State

---
+++ src/main/java/ru/practicum/shareit/booking/State.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.booking;
+/**
+ * Класс описывает State Booking
+ * ALL - все,
+ * WAITING - ожидающие подтверждения,
+ * CURRENT — текущие,
+ * PAST — завершённые,
+ * REJECTED — отклонённые,
+ * FUTURE — будущие.
+ * */
+public enum State {
+    ALL, WAITING, CURRENT, PAST, REJECTED, FUTURE
+}
