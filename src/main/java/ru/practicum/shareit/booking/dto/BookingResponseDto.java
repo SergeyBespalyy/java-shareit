55bbd99b6dca1eefb4dc0b5d475fd72b7cf3b556
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-06-05 17:07:37 +0300

    refactor: добавить конструкторы

--- src/main/java/ru/practicum/shareit/booking/dto/BookingResponseDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingResponseDto.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.Status;
 import ru.practicum.shareit.item.Item;
 import ru.practicum.shareit.user.User;
@@ -14,6 +16,8 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingResponseDto {
 
     private Long id;
