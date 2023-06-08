ca5af14cb90b40a617b460bc976db1652891cb10
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-06-08 13:23:52 +0300

    refactor:добавить дополнительное ДТО для респонса

---
+++ src/main/java/ru/practicum/shareit/item/dto/CommentResponseDto.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+/**
+ * Класс описывает модель CommentDto.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentResponseDto {
+    private Long id;
+
+    @NotBlank(message = "Отзыв не может быть пустым")
+    private String text;
+
+    private Item item;
+
+    private String authorName;
+
+    private LocalDateTime created;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Item {
+        private long id;
+        private String name;
+    }
+}
