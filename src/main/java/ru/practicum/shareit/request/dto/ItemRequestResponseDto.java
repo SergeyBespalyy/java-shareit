b2c986d2c3a7d1cbafce95ac615dd5af92eb9c0f
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-06-08 13:25:40 +0300

    refactor:добавить модель внутренний class Owner для респонса

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestResponseDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestResponseDto.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.item.dto.ItemDtoShort;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -22,5 +22,5 @@ public class ItemRequestResponseDto {
     private Long id;
     private String description;
     private LocalDateTime created;
-    private List<Item> items = new ArrayList<>();
+    private List<ItemDtoShort> items = new ArrayList<>();
 }
